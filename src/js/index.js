1163be2260444827e4e2cd0e2bc7dcf091a1ac6b
buchowski <adam.bucholz@gonitro.com>
2017-07-14 15:23:13 -0700

    get example working

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,20 @@
-import * as THREE from 'three';
-
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 let renderer = new THREE.WebGLRenderer();
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+scene.add(cube);
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+cube.rotation.x += 5;
+animate();
