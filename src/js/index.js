9a4652a555e8139599171dc245cf60ff99d71a76
buchowski <adam.bucholz@gonitro.com>
2017-07-17 15:30:58 -0700

    identify genesis doc

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,17 @@
 let OrbitControls = require('three-orbit-controls')(THREE)
 let docs = require('./dummy-data.json')
 
+const unit = 0.05
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let controls = new OrbitControls(camera)
 let renderer = new THREE.WebGLRenderer();
-let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshLambertMaterial({ color: 0x2194ce, wireframe: false });
+let geometry = new THREE.OctahedronGeometry(unit)
+let materialOne = new THREE.MeshLambertMaterial({ color: 0x2194ce, wireframe: false });
+let materialTwo = new THREE.MeshLambertMaterial({ color: 0xf4416e, wireframe: false })
 let docIds = Object.keys(docs)
 let lights = [];
+let treeData = {}
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xefd1b5)
@@ -28,13 +31,25 @@ scene.add(lights[1]);
 scene.add(lights[2]);
 
 docIds.forEach((id, index) => {
-    let cube = new THREE.Mesh(geometry, material);
+    let doubleUnit = unit * 2
+    let doc = docs[id]
+    let parentId = Number(doc.parentId)
+    let material = materialOne
+    let isGenesisDoc = !doc.parentId
+    let mesh
 
-    docs[id].mesh = cube
-    scene.add(cube);
-    cube.position.z = -1.3 * index
-    cube.position.x = 0.3 * index
-    cube.position.y = 0.3 * index
+    if (isGenesisDoc) {
+        material = materialTwo
+        parentId = docIds.length + 1
+    }
+
+    treeData[parentId] = treeData[parentId] ? ++treeData[parentId] : 1
+
+    mesh = new THREE.Mesh(geometry, material);
+    doc.mesh = mesh
+    scene.add(mesh);
+    mesh.position.x = (treeData[parentId] - 1) * doubleUnit
+    mesh.position.y = (doubleUnit * parentId) - ((docIds.length * doubleUnit) / 2)
 })
 
 function animate() {
