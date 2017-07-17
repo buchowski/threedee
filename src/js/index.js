7e079dab8fe5ecd664c73a6e745b99bffa073ddf
buchowski <adam.bucholz@gonitro.com>
2017-07-17 12:59:08 -0700

    add lights. use lambert mesh

--- src/js/index.js
+++ src/js/index.js
@@ -5,21 +5,35 @@ let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 let controls = new OrbitControls(camera)
 let renderer = new THREE.WebGLRenderer();
 let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0x2194ce, wireframe: false });
+let material = new THREE.MeshLambertMaterial({ color: 0x2194ce, wireframe: false });
+let lights = [];
 let cubes = [];
 let count = 0;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xefd1b5)
 document.body.appendChild(renderer.domElement);
 camera.position.z = 5;
+
+lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+
+lights[0].position.set(0, 200, 0);
+lights[1].position.set(100, 200, 100);
+lights[2].position.set(-100, -200, -100);
+
+scene.add(lights[0]);
+scene.add(lights[1]);
+scene.add(lights[2]);
 
 while (count < 20) {
     let cube = new THREE.Mesh(geometry, material);
 
     scene.add(cube);
-    cube.position.z = -1.3 * count
+    cube.position.z = -10.3 * count
     cube.position.x = 0.3 * count
-    cube.position.y = 0.3 * count
+    cube.position.y = 1.3 * count
     cubes.push(cube);
     count++;
 }
