8e5f64d010d7456fc7d6f4ac5ec368ee5477a4ec
buchowski <adam.bucholz@gonitro.com>
2017-07-19 09:17:25 -0700

    export whichever position algorithm you want to use

--- src/js/positioning.js
+++ src/js/positioning.js
@@ -10,14 +10,14 @@ function isPositionTaken(x, y, z) {
     return takenPositions.indexOf(positionString) !== -1
 }
 
-export default function calcNewPosition(proposedX, proposedY, proposedZ) {
+function calcBlobPosition(proposedX, proposedY, proposedZ) {
     let acceptedX = proposedX
     let acceptedY = proposedY
     let acceptedZ = proposedZ
     let deltaIndex = 0
     let specialYDelta = 0
 
-    while (isPositionTaken(acceptedX, acceptedY, acceptedZ) && deltaIndex < deltas.length) {
+    while (isPositionTaken(acceptedX, acceptedY, acceptedZ)) {
         let [deltaX, deltaY, deltaZ] = deltas[deltaIndex]
 
         acceptedX = proposedX + deltaX * spaceUnit
@@ -35,3 +35,5 @@ export default function calcNewPosition(proposedX, proposedY, proposedZ) {
     return { acceptedX, acceptedY, acceptedZ }
 }
 
+// simply export a different function when you want a different positioning algorithm
+export default calcBlobPosition
